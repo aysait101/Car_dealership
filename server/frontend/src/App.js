8c748425dcb1da8609eea213730097c942a142a5
Abir Sait <aysait101@gmail.com>
2024-07-25 20:03:51 -0400

    week2

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,14 @@
-import LoginPanel from "./components/Login/Login"
+import LoginPanel from "./components/Login/Login";
+import Registerss from "./components/Register/Register";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/register" element={<Registerss />} />
     </Routes>
   );
 }
+
 export default App;
