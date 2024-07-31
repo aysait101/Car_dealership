b3a86654b9398ff010d9ee558b0fb843bf8ac0eb
aysait101 <aysait101@gmail.com>
2024-07-31 11:12:44 +0300

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -5,21 +5,53 @@ from django.conf import settings
 from . import views
 
 app_name = 'djangoapp'
-urlpatterns = [
-    # # path for registration
-    path(route='register', view=views.registration, name='register'),
 
-    # path for login
-    path(route='login', view=views.login_user, name='login'),
-     # path for logout
-    path(route='logout', view=views.logout_request, name='logout'),
-    # path for get_cars
-    #path(route='get_cars', view=views.get_cars, name ='getcars'),
-    # path for dealer reviews view
-    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
-    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
-    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
-    # path for add a review view
-    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
-    path(route='add_review', view=views.add_review, name='add_review'),
+urlpatterns = [
+    # Path for registration
+    path(
+        route='register',
+        view=views.registration,
+        name='register'
+    ),
+    # Path for login
+    path(
+        route='login',
+        view=views.login_user,
+        name='login'
+    ),
+    # Path for logout
+    path(
+        route='logout',
+        view=views.logout_request,
+        name='logout'
+    ),
+    # Path for dealer details view
+    path(
+        route='dealer/<int:dealer_id>',
+        view=views.get_dealer_details,
+        name='dealer_details'
+    ),
+    # Path for getting all dealers or by state
+    path(
+        route='get_dealers',
+        view=views.get_dealerships,
+        name='get_dealers'
+    ),
+    path(
+        route='get_dealers/<str:state>',
+        view=views.get_dealerships,
+        name='get_dealers_by_state'
+    ),
+    # Path for dealer reviews view
+    path(
+        route='reviews/dealer/<int:dealer_id>',
+        view=views.get_dealer_reviews,
+        name='dealer_reviews'
+    ),
+    # Path for adding a review
+    path(
+        route='add_review',
+        view=views.add_review,
+        name='add_review'
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
